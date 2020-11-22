b8bfff32bccd44af8318fa31a0ea5a2eefa60fec
Nathaniel May <nathanieldmay@gmail.com>
2020-11-21 21:21:16 -0500

    text changes to header and footer

--- src/components/Header.js
+++ src/components/Header.js
@@ -8,13 +8,12 @@ const Header = props => (
     </div>
     <div className="content">
       <div className="inner">
-        <h1>Dimension</h1>
+        <h1>Nathaniel May</h1>
         <p>
-          A fully responsive site template designed by{' '}
-          <a href="https://html5up.net">HTML5 UP</a> and released
+          — Engineering Manager —
           <br />
-          for free under the{' '}
-          <a href="https://html5up.net/license">Creative Commons</a> license.
+          <br />
+          Simplicity · Honesty · Community · Creativity · Advocacy
         </p>
       </div>
     </div>
