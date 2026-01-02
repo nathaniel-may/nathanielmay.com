15a9e98cdd9a834b765f94f3c0eb41c788010dbf
Nathaniel May <github@nathanielmay.com>
2026-01-02 14:23:54 +0100

    update wording

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,58 +1,70 @@
 import PropTypes from 'prop-types'
 import React from 'react'
 
-const Header = props => (
+const Header = (props) => (
   <header id="header" style={props.timeout ? { display: 'none' } : {}}>
     <div className="logo">
-      <p style={{"font-weight": "500", "font-size": "30px"}}>λ</p>
+      <p style={{ 'font-weight': '500', 'font-size': '30px' }}>λ</p>
     </div>
     <div className="content">
       <div className="inner">
         <h1>Nathaniel May</h1>
-        <h2>Senior Software Engineer</h2>
+        <h2>Engineering Manager</h2>
         <p>
-          Management Experience · Rust · Data · Math
+          Hiring Pipelines · Coaching · Distributed Systems · Data Analytics
         </p>
       </div>
     </div>
     <nav>
       <ul>
-      <li>
-      <ul className="branch">
-        <li className="leaf">
-          <a href="https://dev.to/codenoodle" title="blog" target="_blank">
-            <div className="logo">
-              <span className="icon fa-book"></span>
-            </div>
-          </a>
+        <li>
+          <ul className="branch">
+            <li className="leaf">
+              <a href="https://dev.to/codenoodle" title="blog" target="_blank">
+                <div className="logo">
+                  <span className="icon fa-book"></span>
+                </div>
+              </a>
+            </li>
+            <li className="leaf">
+              <a
+                href="https://github.com/nathaniel-may"
+                title="github"
+                target="_blank"
+              >
+                <div className="logo">
+                  <span className="icon fa-github"></span>
+                </div>
+              </a>
+            </li>
+          </ul>
         </li>
-        <li className="leaf">
-          <a href="https://github.com/nathaniel-may" title="github" target="_blank">
-            <div className="logo">
-              <span className="icon fa-github"></span>
-            </div>
-          </a>
+        <li>
+          <ul className="branch">
+            <li className="leaf">
+              <a
+                href="https://twitter.com/codenoodle"
+                title="twitter"
+                target="_blank"
+              >
+                <div className="logo">
+                  <span className="icon fa-twitter"></span>
+                </div>
+              </a>
+            </li>
+            <li className="leaf">
+              <a
+                href="https://www.linkedin.com/in/nathanieldmay/"
+                title="linkedin"
+                target="_blank"
+              >
+                <div className="logo">
+                  <span className="icon fa-linkedin"></span>
+                </div>
+              </a>
+            </li>
+          </ul>
         </li>
-      </ul>
-      </li>
-      <li>
-      <ul className="branch">
-        <li className="leaf">
-          <a href="https://twitter.com/codenoodle" title="twitter" target="_blank">
-            <div className="logo">
-              <span className="icon fa-twitter"></span>
-            </div>
-          </a>
-        </li>
-        <li className="leaf">
-          <a href="https://www.linkedin.com/in/nathanieldmay/" title="linkedin" target="_blank">
-            <div className="logo">
-              <span className="icon fa-linkedin"></span>
-            </div>
-          </a>
-        </li>
-      </ul>
-      </li>
       </ul>
     </nav>
   </header>
